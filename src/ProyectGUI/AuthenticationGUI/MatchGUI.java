3188fdbf07eda4faf660a21b1be4647c6025c93a
SantiagoPilamunga <santiago.pilamunga@udla.edu.ec>
2025-01-25 00:22:20 -0500

    Match inicial

--- src/ProyectGUI/AuthenticationGUI/MatchGUI.java
+++ src/ProyectGUI/AuthenticationGUI/MatchGUI.java
@@ -1,4 +1,21 @@
 package ProyectGUI.AuthenticationGUI;
 
+import javax.swing.*;
+
 public class MatchGUI {
+    private JButton rechazarButton;
+    private JButton aceptarButton;
+    private JTextArea txtCarreraFacu;
+    private JTextArea txtEdadNom;
+    private JTextArea txtGustos;
+    private JTextArea txtDescripcion;
+    private JPanel pMatch;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("MatchGUI");
+        frame.setContentPane(new MatchGUI().pMatch);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
